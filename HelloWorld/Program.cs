1821d1941a05b97b5a961c88a1dffd5cec35cc82
Mahreen Qureshi <mahreenq@hotmail.com>
2018-05-25 12:11:04 -0400

    completed strings lesson

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,60 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ALLOWS YOU TO USE QUOTATIONS MARKS IN STRING
+            //string myString = "My \"so-called\" life";
+
+            //LINE BREAK
+            //string myString = "What if I need a \nnew line"; 
+
+            //ALLOWS YOU YO USE BACKSLASH CHARACTERS
+            //string myString = "Go to you c:\\ drive";
+            //string myString = @"Go to you c:\ drive";
+
+            //string myString = String.Format("{0} = {1}", "First", "Second");
+
+            //FORMATS TO CURRENCY
+            //string myString = string.Format("{0:C}", 123.45);
+
+            //FORMATS NUMBER SO IT IS EASY TO READ
+            //string myString = string.Format("{0:N}", 1234567890);
+
+            //FORMATS TO PERCENTAGE
+            //string myString = string.Format("Percentage: {0:P}", 0.123);
+
+            //CUSTOM FORMAT
+            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);
+
+            string myString = " Summer is here yaaaaaayyy  ";
+            //StART AT 6TH CHAR, END AT 10TH CHAR
+            //myString = myString.Substring(6, 10);
+            //TO UPPERCASE
+            //myString = myString.ToUpper();
+            //REPLACE CHARACTERS
+            //myString = myString.Replace(" ", "--");
+            //REMOVES CHARACTERS FROM STRING
+            //myString = myString.Remove(6, 10);
+
+            myString = String.Format("Length before: {0} -- Length after: {1}", myString.Length, myString.Trim().Length);
+
+            /*  string myString = "";
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            }
+            */
+            /*  
+             StringBuilder myString = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+             {
+                 myString.Append("--");
+                 myString.Append(i);
+             }
+            */
+
+
+
+            Console.WriteLine(myString);
             Console.ReadLine();
         }
     }
--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 <li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/HelloWorld/HelloWorld/Program.cs">HelloWorld </a> </li>
 <li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/Variables/HelloWorld/Program.cs">Variables in C# </a> </li>
 <li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/ifStatements/HelloWorld/Program.cs">If Statements / Decisions </a> </li>
-<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/assessment/HelloWorld/Program.cs">CHEATSHEAT - Assessment: Operators, Expressions and Statements </a> </li>
-<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/iteration/HelloWorld/Program.cs">Assessment: for Iteration Statement </a> </li>
+<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/assessment/HelloWorld/Program.cs">Cheatsheet - Operators, Expressions and Statements </a> </li>
+<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/iteration/HelloWorld/Program.cs">for Iteration Statement </a> </li>
 <li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/arrays/HelloWorld/Program.cs">Understanding Arrays </a> </li>
-<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/methods/HelloWorld/Program.cs">Assessment: Defining and Calling Methods </a> </li>
-<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/while/HelloWorld/Program.cs">Assessment: While Iteration Statement </a> </li>
+<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/methods/HelloWorld/Program.cs">Defining and Calling Methods </a> </li>
+<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/while/HelloWorld/Program.cs">While Iteration Statement </a> </li>
+<li> <a href="https://github.com/mahreenq/CSharpTutorial/blob/strings/HelloWorld/Program.cs">Working with Strings </a> </li>
 </ol>
