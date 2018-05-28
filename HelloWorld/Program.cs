6e68637ac335bbe80cc1d421d974f7e20b502af7
Mahreen Qureshi <mahreenq@hotmail.com>
2018-05-28 13:40:20 -0400

    completed classes II lesson

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,77 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Car myCar = new Car();
+
+            Car.MyMethod();
+
+            /*
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 2000;
+            myCar.Colour = "Black";
+
+
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "white");
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                              myOtherCar.Make,
+                              myOtherCar.Model,
+                              myOtherCar.Year,
+                              myOtherCar.Colour);
+
+            myOtherCar.Model = "98";
+
+
+            
+             Console.WriteLine("{0} {1} {2} {3}",
+                  myCar.Make,
+                  myCar.Model,
+                  myCar.Year,
+                  myCar.Colour);
+
+
+            /*Console.WriteLine("{0} {1} {2} {3}",
+                  myOtherCar.Make,
+                  myOtherCar.Model,
+                  myOtherCar.Year,
+                  myOtherCar.Colour);
+            
+            */
+
             Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+
+        /*
+        public Car()
+        {
+            this.Make = "Nissan";
+        }
+
+
+        //OVERLOADED CONSTRUCTOR
+        public Car (string make, string model, int year, string colour)
+        {
+            Make = make;
+            Model = Model;
+            Year = year;
+            Colour = colour;
+        }
+        */
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Called the static MyMethod");
+        }
+
+
     }
 }
