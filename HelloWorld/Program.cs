8c0a63a59698cc0934152ef4066e45de91627c82
Mahreen Qureshi <mahreenq@hotmail.com>
2018-05-28 10:32:28 -0400

    completed date/time lesson

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthday = new DateTime(2000, 12, 15);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("10/23/2002");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
             Console.ReadLine();
         }
     }
